a0f816280144f238281f1f764a165e81463ef5be
Galantsev, Dmitrii <dmitrii.galantsev@amd.com>
2024-02-06 20:30:57 -0600

    Add amdsmi
    Signed-off-by: Galantsev, Dmitrii <dmitrii.galantsev@amd.com>

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
+#include "functions/access_types.hpp"
+#include "functions/bad_std_copy.hpp"
 #include "functions/smart_pointers.hpp"
+#include "functions/try_amdsmi.hpp"
 
 int main() {
     int status = 0;
     // status += access_types();
     // status += bad_std_copy();
-    status += smart_pointers();
+    // status += smart_pointers();
+    status += try_amdsmi();
 
     return status;
 }
