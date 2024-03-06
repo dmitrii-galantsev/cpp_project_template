9901f30311ff7a1542b2b6bacfb6a8571f587385
Galantsev, Dmitrii <dmitrii.galantsev@amd.com>
2024-03-05 23:36:49 -0600

    Add try_scandir
    Signed-off-by: Galantsev, Dmitrii <dmitrii.galantsev@amd.com>

--- main.cpp
+++ main.cpp
@@ -2,13 +2,15 @@
 #include "functions/bad_std_copy.hpp"
 #include "functions/smart_pointers.hpp"
 #include "functions/try_amdsmi.hpp"
+#include "functions/try_scandir.hpp"
 
 int main() {
     int status = 0;
     // status += access_types();
     // status += bad_std_copy();
     // status += smart_pointers();
-    status += try_amdsmi();
+    // status += try_amdsmi();
+    status += try_scandir();
 
     return status;
 }
