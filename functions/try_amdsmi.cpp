f0734da83a95ee453728273f361d5acca3468276
Galantsev, Dmitrii <dmitrii.galantsev@amd.com>
2025-07-21 19:30:22 -0500

    Enum CPUs GPUs and CPU cores
    Signed-off-by: Galantsev, Dmitrii <dmitrii.galantsev@amd.com>

--- functions/try_amdsmi.cpp
+++ functions/try_amdsmi.cpp
@@ -7,11 +7,12 @@
 
 #include "amd_smi/amdsmi.h"
 
-#define LOG(X)                                                                               \
-    do {                                                                                     \
-        if ((X) != AMDSMI_STATUS_SUCCESS) {                                                  \
-            std::cout << "AMDSMI ERROR: " << status << " [" << __LINE__ << "]" << std::endl; \
-        }                                                                                    \
+#define LOG(X)                                                                          \
+    do {                                                                                \
+        if ((X) != AMDSMI_STATUS_SUCCESS) {                                             \
+            std::cout << __FUNCTION__ << ":" << __LINE__ << " AMDSMI ERROR: " << status \
+                      << std::endl;                                                     \
+        }                                                                               \
     } while (0)
 
 amdsmi_status_t get_processor_handle_from_id(uint32_t gpu_id,
@@ -90,41 +91,90 @@ auto fetch_smi_field(uint32_t gpu_index) {
 
 int try_amdsmi() {
     amdsmi_status_t status = AMDSMI_STATUS_UNKNOWN_ERROR;
-    status = amdsmi_init(AMDSMI_INIT_AMD_GPUS);
+    status = amdsmi_init(AMDSMI_INIT_AMD_CPUS | AMDSMI_INIT_AMD_GPUS);
     LOG(status);
+
     uint32_t socket_count = 0;
     status = amdsmi_get_socket_handles(&socket_count, nullptr);
     LOG(status);
     std::cout << "Socket count: " << socket_count << std::endl;
-
     std::vector<amdsmi_socket_handle> sockets(socket_count);
     status = amdsmi_get_socket_handles(&socket_count, sockets.data());
     LOG(status);
+
+    // return 0;
     for (auto& socket : sockets) {
-        char name[50];
-        status = amdsmi_get_socket_info(socket, 50, name);
-        std::cout << "  Socket name: " << name << "\n";
+        std::cout << "Socket handle: " << socket << "\n";
 
-        uint32_t processor_count = 0;
-        status = amdsmi_get_processor_handles(socket, &processor_count, nullptr);
+        uint32_t gpus_per_soc = 0;
+        status = amdsmi_get_processor_handles_by_type(socket, AMDSMI_PROCESSOR_TYPE_AMD_GPU,
+                                                      nullptr, &gpus_per_soc);
         LOG(status);
-        std::cout << "  Processor count: " << processor_count << "\n";
+        std::cout << "  GPU per soc count: " << gpus_per_soc << "\n";
+        std::vector<amdsmi_processor_handle> gpus(gpus_per_soc);
+        status = amdsmi_get_processor_handles_by_type(socket, AMDSMI_PROCESSOR_TYPE_AMD_GPU,
+                                                      gpus.data(), &gpus_per_soc);
+        LOG(status);
 
-        std::vector<amdsmi_processor_handle> processors(processor_count);
-        status = amdsmi_get_processor_handles(socket, &processor_count, processors.data());
+        uint32_t cpus_per_soc = 0;
+        status = amdsmi_get_processor_handles_by_type(socket, AMDSMI_PROCESSOR_TYPE_AMD_CPU,
+                                                      nullptr, &cpus_per_soc);
+        LOG(status);
+        std::cout << "  CPU per soc count: " << cpus_per_soc << "\n";
+        std::vector<amdsmi_processor_handle> cpus(cpus_per_soc);
+        status = amdsmi_get_processor_handles_by_type(socket, AMDSMI_PROCESSOR_TYPE_AMD_CPU,
+                                                      cpus.data(), &cpus_per_soc);
+        LOG(status);
 
-        for (auto& processor : processors) {
-            processor_type_t processor_type = AMDSMI_PROCESSOR_TYPE_UNKNOWN;
-            status = amdsmi_get_processor_type(processor, &processor_type);
-            std::cout << "    Processor type: " << processor_type << "\n";
-            amdsmi_board_info_t board_info;
-            status = amdsmi_get_gpu_board_info(processor, &board_info);
-            std::cout << "    Board name: " << board_info.model_number << "\n";
-            amdsmi_asic_info_t info;
-            status = amdsmi_get_gpu_asic_info(processor, &info);
-            std::cout << "    Asic info: " << info.market_name << "\n";
+        uint32_t cores_per_soc = 0;
+        status = amdsmi_get_processor_handles_by_type(socket, AMDSMI_PROCESSOR_TYPE_AMD_CPU_CORE,
+                                                      nullptr, &cores_per_soc);
+        LOG(status);
+        std::cout << "  CPU_CORE per soc count: " << cores_per_soc << "\n";
+        std::vector<amdsmi_processor_handle> cores(cores_per_soc);
+        status = amdsmi_get_processor_handles_by_type(socket, AMDSMI_PROCESSOR_TYPE_AMD_CPU_CORE,
+                                                      cores.data(), &cores_per_soc);
+        LOG(status);
+        continue;
+
+        // GPUs
+        for (auto& gpu : gpus) {
+            std::cout << "  GPU handle: " << gpu << "\n";
+            char gpu_name[AMDSMI_MAX_STRING_LENGTH] = {};
+            status = amdsmi_get_gpu_vendor_name(gpu, gpu_name, AMDSMI_MAX_STRING_LENGTH);
+            LOG(status);
+            std::cout << "    GPU vendor name: " << gpu_name << "\n";
         }
-        std::cout << "\n";
+
+        // CPUs
+        for (auto& cpu : cpus) {
+            std::cout << "  CPU handle: " << cpu << "\n";
+            amdsmi_cpu_info_t cpu_info = {};
+            status = amdsmi_get_cpu_model_name(cpu, &cpu_info);
+            LOG(status);
+            std::cout << "    CPU name: " << cpu_info.model_name << "\n";
+        }
+
+        // CORES
+        for (auto& core : cores) {
+            std::cout << "  Core handle: " << core << "\n";
+            uint32_t boostlimit = 0;
+            status = amdsmi_get_cpu_core_boostlimit(core, &boostlimit);
+            LOG(status);
+            if (status == AMDSMI_STATUS_SUCCESS)
+                std::cout << "    Boostlimit: " << boostlimit << "\n";
+
+            uint32_t freq = 0;
+            status = amdsmi_get_cpu_core_current_freq_limit(core, &freq);
+            LOG(status);
+            if (status == AMDSMI_STATUS_SUCCESS)
+                std::cout << "    Current frequency limit: " << freq << " MHz\n";
+
+            uint64_t energy = 0;
+            status = amdsmi_get_cpu_core_energy(core, &energy);
+            // LOG(status);
+            if (status == AMDSMI_STATUS_SUCCESS) std::cout << "    Energy: " << energy << " nJ\n";
+        }
     }
 
     status = amdsmi_shut_down();
